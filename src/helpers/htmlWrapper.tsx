9d3232b98c9e4c1538446060bd29a656e5488272
Tylor Shin <shincode@icloud.com>
2016-11-12 00:15:54 +0900

    Changed document title as our blog name 'EmoLogic'

--- src/helpers/htmlWrapper.tsx
+++ src/helpers/htmlWrapper.tsx
@@ -9,7 +9,7 @@ export function staticHTMLWrapper(
     <html>
       <head>
         <meta charset="utf-8">
-        <title>Vingle Desktop</title>
+        <title>EmoLogic</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
       </head>
       <body>
