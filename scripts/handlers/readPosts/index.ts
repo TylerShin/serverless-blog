c3500871da217047f52327c4c4ccbf9064b1d59a
Tylor Shin <shincode@icloud.com>
2016-12-08 09:00:59 +0900

    Added scan posts

--- scripts/handlers/readPosts/index.ts
+++ scripts/handlers/readPosts/index.ts
@@ -7,22 +7,23 @@ const POSTS_COUNT_PER_PAGE: number = 3;
 export default function handler(event: any, context: any) {
   const params = {
     TableName: DYNAMO_DB_TABLE_NAME,
-    // KeyConditionExpression: "#email = :emailValue and #timestamp BETWEEN :from AND :to",
-    // ExpressionAttributeNames: {
-    //   "#email": "email",
-    //   "#timestamp": "timestamp"
-    // },
-    Limit: POSTS_COUNT_PER_PAGE,
-    // ExpressionAttributeValues: {
-    // ":emailValue": email,
-    // ":from": from.getTime(),
-    // ":to": to.getTime()
-    // },
   };
 
-  dynamodb.query(params, function (err: Error, data: any) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else console.log(data);           // successful response
+  dynamodb.scan(params, function (err: Error, data: any) {
+    if (err) {
+      console.error(err, err.stack);
+    }
+    else {
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "Access-Control-Allow-Origin": "*",
+          "Cache-control": "max-age=300",
+        },
+        body: data.Items,
+      };
+      context.done(null, response);
+    }
   });
 };
-
